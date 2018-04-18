350f9d65ff190aae9a622efb9d9df2652a91dbc3
Avtandil Kikabidze <akalongman@gmail.com>
2018-04-18 23:51:37 +0400

    Add more items to the config

--- src/config/config.php
+++ src/config/config.php
@@ -5,13 +5,40 @@ return [
      * Bot configuration
      */
     'bot' => [
-        'name'    => env('PHP_TELEGRAM_BOT_NAME', ''),
-        'api_key' => env('PHP_TELEGRAM_BOT_API_KEY', ''),
+        'name'    => env('PHP_TELEGRAM_BOT_NAME', 'Test Name'),
+        'api_key' => env('PHP_TELEGRAM_BOT_API_KEY', '489567508:AAEJgd80WMYbl7NisJYpkWCJLt2o_0RkpXs'),
     ],
 
+    /**
+     * Database integration
+     */
     'database' => [
         'enabled'    => false,
-        'connection' => 'default',
+        'connection' => env('DB_CONNECTION', 'mysql'),
     ],
 
+    'commands' => [
+        'before'  => true,
+        'paths'   => [
+            // Custom command paths
+        ],
+        'configs' => [
+            // Custom commands configs
+        ],
+    ],
+
+    'admins' => [
+        // Admin ids
+    ],
+
+    /**
+     * Request limiter
+     */
+    'limiter' => [
+        'enabled'  => false,
+        'interval' => 1,
+    ],
+
+    'upload_path'   => '',
+    'download_path' => '',
 ];

acdea980cb7bb16890123630ad11f319e11b4fa6
Pooria <pooria.morovati@gmail.com>
2020-10-29 17:04:04 +0330

    Added first_name and last_name columns

--- src/database/migrations/2018_04_18_193554_create_chat_table.php
+++ src/database/migrations/2018_04_18_193554_create_chat_table.php
@@ -14,6 +14,8 @@ class CreateChatTable extends Migration
             $table->enum('type', ['private', 'group', 'supergroup', 'channel'])->comment('Chat type, either private, group, supergroup or channel');
             $table->char('title')->nullable()->default('')->comment('Chat (group) title, is null if chat type is private');
             $table->char('username')->nullable()->comment('Username, for private chats, supergroups and channels if available');
+            $table->char('first_name')->nullable()->comment('First name of the other party in a private chat');
+            $table->char('last_name')->nullable()->comment('Last name of the other party in a private chat');
             $table->boolean('all_members_are_administrators')->nullable()->default(0)->comment('True if a all members of this group are admins');
             $table->timestamps();
             $table->bigInteger('old_id')->nullable()->index('old_id')->comment('Unique chat identifier, this is filled when a group is converted to a supergroup');

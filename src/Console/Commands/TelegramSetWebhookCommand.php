93549a0563bf48412cecf7e309d750d847a9395e
Tii <mail@tii.one>
2022-04-20 15:58:26 +0200

    Revert Migration

--- src/Console/Commands/TelegramSetWebhookCommand.php
+++ src/Console/Commands/TelegramSetWebhookCommand.php
@@ -4,13 +4,15 @@ namespace Tii\LaravelTelegramBot\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Telegram;
 
 class TelegramSetWebhookCommand extends Command
 {
     protected $signature = 'telegram:set-webhook
                             {hostname? : Hostname to set}
-                            {--d|drop-pending-updates : Pass to drop all pending updates}';
+                            {--d|drop-pending-updates : Pass to drop all pending updates}
+                            {--a|all-update-types : Passes all possible update types to allowed_updates}';
 
     protected $description = 'Use this method to specify a url and receive incoming updates via an outgoing webhook';
 
@@ -36,6 +38,10 @@ class TelegramSetWebhookCommand extends Command
         $options = [];
         if ($this->option('drop-pending-updates')) {
             $options['drop_pending_updates'] = true;
+        }
+
+        if ($this->option('all-update-types')) {
+            $options['allowed_updates'] = Update::getUpdateTypes();
         }
 
         $response = $bot->setWebhook($url, $options);
